2ab8597ae43f505c6319fe0b7349f46f45f91569
KrzysztofPajak <krzysiek@nop4you.com>
2019-09-05 09:54:34 +0200

    Add project files.

---
+++ Soneta_Sample_RS/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+using Soneta.Business.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soneta_Sample_RS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Soneta.Start.Loader loader = new Soneta.Start.Loader();
+            loader.WithExtensions = false;
+            loader.Load();
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Login login = CreateLogin();
+            using (Session session = login.CreateSession(false, false))
+            {
+                var context = Context.Empty.Clone(session);
+                var storage = context.Login != null ? context.Login.StorageProvider : null;
+                IReportService rs;
+                context.Session.GetService(out rs);
+            }
+        }
+
+        public Login CreateLogin()
+        {
+            Login login;
+            MsSqlDatabase msdb = new MsSqlDatabase("enova", @"DESKTOP-LDPQC0R\SQL2017", "enova", "sa", "sa", false);
+            msdb.Active = true;
+            try
+            {
+                login = msdb.Login(false, "Administrator", "123456");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            return login;
+        }
+    }
+}
